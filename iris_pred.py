7d2e65f87e546ab4a818484648ee959d196b52a3
Elsa Wang <square0603@gmail.com>
2024-11-14 14:52:48 +0800

    Add files via upload

--- iris_pred.py
+++ iris_pred.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import joblib
 
-def page2():
-    st.title("Second page")
-
-# https://docs.streamlit.io/develop/api-reference/status
-# https://tw.piliapp.com/emoji/list/
+# streamlit: https://docs.streamlit.io/develop/api-reference/status
+# 圖示: https://tw.piliapp.com/emoji/list/
 pg = st.navigation([
-    st.Page("page1.py", title="IRIS 品種預測", icon="🌹"),
-    st.Page("page2.py", title="IRIS 相關資訊", icon=":material/favorite:"),
+    st.Page("pages/page1.py", title="IRIS 品種預測", icon="🌹"),
+    st.Page("pages/page2.py", title="IRIS 相關資訊", icon=":material/favorite:"),
 ])
 pg.run()
 
