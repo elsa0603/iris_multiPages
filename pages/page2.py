a251a9e73b660504607d4dc4ae0559d1d71d34b0
Elsa Wang <square0603@gmail.com>
2024-11-14 15:17:18 +0800

    Add files via upload

--- pages/page2.py
+++ pages/page2.py
@@ -4,35 +4,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("IRIS 相關資訊")
-
 st.write("依'花萼長寬'繪製散點圖：")
 df = pd.read_csv("iris.csv")
+
+fig, ax = plt.subplots()
+# species_map = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+species_map = {'Setosa':0 , 'Versicolor':1 , 'Virginica':2 }
+colors = ['red', 'green', 'blue']
+
+#分Tab顯示不同欄位的圖
+tab1, tab2 = st.tabs(["依花萼長寬", "依花瓣長寬"])
+
+tab1.write("依花萼長寬顯示樣本分佈情形")
+with tab1:
+    for i, species in species_map.items():
+        subset = df[df['variety'] == i]  
+        ax.scatter(subset['sepal.length'], subset['sepal.width'],label=i)#, color=colors[i]
+
+    ax.set_xlabel('Sepal Length (cm)')
+    ax.set_ylabel('Sepal Width (cm)')
+    ax.legend()
+    st.pyplot(fig)
+
+fig2, ax2 = plt.subplots()
+tab2.write("依花瓣長寬顯示樣本分佈情形")
+with tab2:
+    for i, species in species_map.items():
+        subset = df[df['variety'] == i]  
+        ax2.scatter(subset['petal.length'], subset['petal.width'],label=i)#, color=colors[i]
+
+    ax2.set_xlabel('Petal Length (cm)')
+    ax2.set_ylabel('Petal Width (cm)')
+    ax2.legend()
+    st.pyplot(fig2)
+
 # df['variety'] = df['variety'].map({0: "Setosa", 1: "Versicolor", 2: 'Virginica'})
 # st.write(df['variety'])
 
-
-# Create the scatter plot using matplotlib.
-fig, ax = plt.subplots()
-
-# Create a dictionary that maps the target numbers (0, 1, 2) to the species names.
-# species_map = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
-species_map = {'Setosa':0 , 'Versicolor':1 , 'Virginica':2 }
-
-# Define colors for each species.
-colors = ['red', 'green', 'blue']
-
-# Plot each species with its own color.
-for i, species in species_map.items():
-    subset = df[df['variety'] == i]  # Get the subset of data for species i
-    ax.scatter(subset['sepal.length'], subset['sepal.width'],label=i)#, color=colors[i]
-
-# Set the axis labels and show the legend.
-ax.set_xlabel('Sepal Length (cm)')
-ax.set_ylabel('Sepal Width (cm)')
-ax.legend()
-
-# Display the plot in the Streamlit app.
-st.pyplot(fig)
 
 
 st.write("繪製線條圖：")
